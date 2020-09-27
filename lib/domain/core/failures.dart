c4516f3552935b22fea1cffea503f5d282805e39
Nifemi Omotosho-Diffu <thenifemi@gmail.com>
2020-09-26 23:40:50 -0300

    ..

---
+++ lib/domain/core/failures.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'failures.freezed.dart';
+
+@freezed
+abstract class ValueFailure<T> with _$ValueFailure<T> {
+  const factory ValueFailure.invalidEmail({
+    @required String failedValue,
+  }) = InvalidEmail<T>;
+
+  const factory ValueFailure.shortPassword({
+    @required String failedValue,
+  }) = ShortPassword<T>;
+}
