5471621a35f59098df56f51b3aa1c46c9c2a47d1
Nifemi Omotosho-Diffu <thenifemi@gmail.com>
2020-09-28 16:26:49 -0300

    ..

--- lib/domain/auth/auth_failure.dart
+++ lib/domain/auth/auth_failure.dart
@@ -6,6 +6,7 @@ part 'auth_failure.freezed.dart';
 abstract class AuthFailure with _$AuthFailure {
   const factory AuthFailure.cancelledByUser() = CancelledByUser;
   const factory AuthFailure.serverError() = ServerError;
+  const factory AuthFailure.emailAlreadyInUse() = EmailAlreadyInUse;
   const factory AuthFailure.invalidEmailAndPasswordCombination() =
       InvalidEmailAndPasswordCombination;
 }
