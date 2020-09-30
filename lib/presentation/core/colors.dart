c28db66cf2a238c5528f86eb9b5fa60c97084161
Nifemi Omotosho-Diffu <thenifemi@gmail.com>
2020-09-30 20:22:05 -0300

    ..

--- lib/presentation/core/colors.dart
+++ lib/presentation/core/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primaryDark = Color(0xFF070706);
+  static const Color primaryDark = Color(0xFF2C2B4B);
   static const Color primarygray = Color(0xFF343434);
   static const Color primaryOrange = Color(0xFFf5cb5c);
 }
