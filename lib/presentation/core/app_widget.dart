030ba0035621111c4dc7b14b808a08906a3f5dcb
Nifemi Omotosho-Diffu <thenifemi@gmail.com>
2020-10-04 22:47:57 -0300

    .

--- lib/presentation/core/app_widget.dart
+++ lib/presentation/core/app_widget.dart
@@ -1,11 +1,11 @@
+import 'package:Jottr/presentation/routes/router.gr.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:Jottr/application/auth/auth_bloc.dart';
 
 import '../../application/auth/auth_bloc.dart';
 import '../../injection.dart';
-import '../sign_in/sign_in_screen.dart';
 import 'colors.dart';
 
 class AppWidget extends StatelessWidget {
@@ -19,25 +19,29 @@ class AppWidget extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
-        title: 'Jottr',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primaryColor: AppColors.primarySmoke,
-          accentColor: Colors.white30,
-          inputDecorationTheme: InputDecorationTheme(
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10.0),
-              borderSide: BorderSide.none,
+          title: 'Jottr',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primaryColor: AppColors.primarySmoke,
+            accentColor: Colors.white30,
+            inputDecorationTheme: InputDecorationTheme(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                borderSide: BorderSide.none,
+              ),
             ),
           ),
-        ),
-        home: AnnotatedRegion<SystemUiOverlayStyle>(
-            value: const SystemUiOverlayStyle(
-              statusBarColor: Colors.transparent,
-              statusBarIconBrightness: Brightness.dark,
-            ),
-            child: SignInScreen()),
-      ),
+          builder: ExtendedNavigator(
+            router: MyRouter(),
+          )
+          // builder: AnnotatedRegion<SystemUiOverlayStyle>(
+          //   value: const SystemUiOverlayStyle(
+          //     statusBarColor: Colors.transparent,
+          //     statusBarIconBrightness: Brightness.dark,
+          //   ),
+          //   child: SignInScreen(),
+          // ),
+          ),
     );
   }
 }
--- lib/presentation/routes/router.dart
+++ lib/presentation/routes/router.dart
@@ -1,9 +1,12 @@
+import 'package:Jottr/presentation/splash/splash_screen.dart';
 import 'package:auto_route/auto_route_annotations.dart';
 import 'package:Jottr/presentation/sign_in/sign_in_screen.dart';
 
 @MaterialAutoRouter(
+  routes: <AutoRoute>[
+    CupertinoRoute(page: SplashScreen, initial: true),
+    CupertinoRoute(page: SignInScreen),
+  ],
   generateNavigationHelperExtension: true,
 )
-class $Router {
-  SignInScreen signInScreen;
-}
+class $MyRouter {}
