4b3ba1ea8ea5e4b8e61f3e06c05316fafef014a8
Nifemi Omotosho-Diffu <thenifemi@gmail.com>
2020-10-07 15:18:31 -0300

    .

--- lib/domain/notes/value_objects.dart
+++ lib/domain/notes/value_objects.dart
@@ -1,1 +1,39 @@
+import 'package:Jottr/domain/core/value_validators.dart';
+import 'package:dartz/dartz.dart';
 
+import '../core/failures.dart';
+import '../core/value_objects.dart';
+
+class NoteBody extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+
+  static const maxLength = 1000;
+
+  factory NoteBody(String input) {
+    assert(input != null);
+    return NoteBody._(
+      validateMaxStringLength(input, maxLength).flatMap(validateStringNotEmpty),
+    );
+  }
+
+  const NoteBody._(this.value);
+}
+
+class TodoName extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+
+  static const maxLength = 30;
+
+  factory TodoName(String input) {
+    assert(input != null);
+    return TodoName._(
+      validateMaxStringLength(input, maxLength)
+          .flatMap(validateStringNotEmpty)
+          .flatMap(validateSingeLine),
+    );
+  }
+
+  const TodoName._(this.value);
+}
