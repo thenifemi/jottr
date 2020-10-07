c38947c8de6273dbe0f3884d96acc24b84f24a28
Nifemi Omotosho-Diffu <thenifemi@gmail.com>
2020-10-07 18:32:36 -0300

    .

--- lib/domain/notes/value_objects.dart
+++ lib/domain/notes/value_objects.dart
@@ -1,7 +1,9 @@
 import 'dart:ui';
 
+import 'package:Jottr/domain/core/value_tranformers.dart';
 import 'package:Jottr/domain/core/value_validators.dart';
 import 'package:dartz/dartz.dart';
+import 'package:kt_dart/collection.dart';
 
 import '../core/failures.dart';
 import '../core/value_objects.dart';
@@ -44,7 +46,7 @@ class NoteColor extends ValueObject<Color> {
   @override
   final Either<ValueFailure<Color>, Color> value;
 
-  static const List<Color> prefdefinedColors = [
+  static const List<Color> predefinedColors = [
     Color(0xff264653), //Charcaol
     Color(0xff2a9d8f), //Persian Green
     Color(0xffe9c46a), //Orange Yellow Crayola
@@ -56,10 +58,32 @@ class NoteColor extends ValueObject<Color> {
 
   factory NoteColor(Color input) {
     assert(input != null);
-    return NoteColor._(
-        //insert validation
-        );
+    return NoteColor._(right(makeColorOpaque(input)));
   }
 
   const NoteColor._(this.value);
+}
+
+class List3<T> extends ValueObject<KtList<T>> {
+  @override
+  final Either<ValueFailure<KtList<T>>, KtList<T>> value;
+
+  static const maxLength = 3;
+
+  factory List3(KtList<T> input) {
+    assert(input != null);
+    return List3._(
+      validateMaxListLength(input, maxLength),
+    );
+  }
+
+  const List3._(this.value);
+
+  int get length {
+    return value.getOrElse(() => emptyList()).size;
+  }
+
+  bool get isfull {
+    return length == maxLength;
+  }
 }
