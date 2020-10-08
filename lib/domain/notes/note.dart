d8e09b95e6bb97565f5ab0339c354c3e1285e7f3
Nifemi Omotosho-Diffu <thenifemi@gmail.com>
2020-10-08 19:13:58 -0300

    .

--- lib/domain/notes/note.dart
+++ lib/domain/notes/note.dart
@@ -2,6 +2,7 @@ import 'package:Jottr/domain/core/value_objects.dart';
 import 'package:Jottr/domain/notes/todo_item.dart';
 import 'package:Jottr/domain/notes/value_objects.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:kt_dart/kt.dart';
 
 part 'note.freezed.dart';
 
@@ -13,4 +14,11 @@ abstract class Note with _$Note {
     @required NoteColor color,
     @required List3<TodoItem> todos,
   }) = _Note;
+
+  factory Note.empty() => Note(
+        id: UniqueId(),
+        body: NoteBody(''),
+        color: NoteColor(NoteColor.predefinedColors[0]),
+        todos: List3(emptyList()),
+      );
 }
