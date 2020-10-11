8e03ddc1d5cb3dacf65607d935d0b657883266c9
Nifemi Omotosho-Diffu <thenifemi@gmail.com>
2020-10-10 21:08:30 -0300

    .

--- lib/domain/notes/i_note_repository.dart
+++ lib/domain/notes/i_note_repository.dart
@@ -5,12 +5,9 @@ import 'note.dart';
 import 'note_failure.dart';
 
 abstract class INoteRepository {
-  //watch notes
-  //watch uncompleted notes
-  //CUD
-  //C Read UD
-
   Stream<Either<NoteFailure, KtList<Note>>> watchAll();
   Stream<Either<NoteFailure, KtList<Note>>> watchUncompleted();
-  Future<Either<NoteFailure, Unit>> create();
+  Future<Either<NoteFailure, Unit>> create(Note note);
+  Future<Either<NoteFailure, Unit>> update(Note note);
+  Future<Either<NoteFailure, Unit>> delete(Note note);
 }
