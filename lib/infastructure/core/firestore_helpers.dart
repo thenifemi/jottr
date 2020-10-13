003cbe6a86069d421e8ada88e376d2784d76252d
Nifemi Omotosho-Diffu <thenifemi@gmail.com>
2020-10-13 11:16:23 -0300

    .

---
+++ lib/infastructure/core/firestore_helpers.dart
@@ -0,0 +1,10 @@
+import 'package:Jottr/domain/auth/i_auth_facade.dart';
+import 'package:Jottr/injection.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+extension FirestoreX on FirebaseFirestore {
+  Future<DocumentReference> userDocument() async {
+    final userOption = await getIt<IAuthFacade>().getSignedInUser();
+    // final user = userOption.getOrElse(() => throw);
+  }
+}
