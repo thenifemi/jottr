1cdd6d2ea6b6d11f6f80b1ec4d9bba2554482187
Nifemi Omotosho-Diffu <thenifemi@gmail.com>
2020-10-13 11:00:32 -0300

    .

---
+++ lib/infastructure/note/note_repository.dart
@@ -0,0 +1,44 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dartz/dartz.dart';
+import 'package:injectable/injectable.dart';
+import 'package:kt_dart/kt.dart';
+
+import '../../domain/notes/i_note_repository.dart';
+import '../../domain/notes/note.dart';
+import '../../domain/notes/note_failure.dart';
+
+@LazySingleton(as: INoteRepository)
+class NoteRepository implements INoteRepository {
+  final FirebaseFirestore _firestore;
+
+  NoteRepository(this._firestore);
+
+  @override
+  Future<Either<NoteFailure, Unit>> create(Note note) {
+    // TODO: implement create
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<NoteFailure, Unit>> delete(Note note) {
+    // TODO: implement delete
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<NoteFailure, Unit>> update(Note note) {
+    // TODO: implement update
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<Either<NoteFailure, KtList<Note>>> watchAll() {
+    // user/{user ID}/notes/{notes ID}
+  }
+
+  @override
+  Stream<Either<NoteFailure, KtList<Note>>> watchUncompleted() {
+    // TODO: implement watchUncompleted
+    throw UnimplementedError();
+  }
+}
