d7335d4f4b1f13c8f863b2096421c9d176cecd43
Nifemi Omotosho-Diffu <thenifemi@gmail.com>
2020-10-16 13:00:59 -0300

    .

--- lib/presentation/notes/notes_overview/notes_overview_screen.dart
+++ lib/presentation/notes/notes_overview/notes_overview_screen.dart
@@ -1,4 +1,7 @@
 import 'package:Jottr/application/auth/auth_bloc.dart';
+import 'package:Jottr/application/notes/note_actor/note_actor_bloc.dart';
+import 'package:Jottr/application/notes/note_watcher/note_watcher_bloc.dart';
+import 'package:Jottr/injection.dart';
 import 'package:Jottr/presentation/core/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -6,41 +9,59 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class NotesOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Jottr',
-          style: TextStyle(
-            color: AppColors.primaryDark,
-            fontSize: 24.0,
-          ),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<NoteWatcherBloc>(
+          create: (context) => getIt<NoteWatcherBloc>()
+            ..add(const NoteWatcherEvent.watchAllStarted()),
         ),
-        actions: [
-          const IconButton(
-              icon: Icon(
-                Icons.check_box_rounded,
-                color: AppColors.primaryDark,
-              ),
-              onPressed: null),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10.0),
-            child: GestureDetector(
-              onTap: () {
-                context.bloc<AuthBloc>().add(const AuthEvent.signedOut());
-              },
-              child: const CircleAvatar(),
-            ),
+        BlocProvider<NoteActorBloc>(
+          create: (context) => getIt<NoteActorBloc>(),
+        ),
+      ],
+      child: MultiBlocListener(
+        listeners: [
+          BlocListener<AuthBloc, AuthState>(
+            listener: (context, state) {},
           ),
         ],
-        elevation: 0.0,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          //TODO: Navigate to NoteFormScreen
-        },
-        child: const Icon(
-          Icons.add,
-          color: Colors.white,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              'Jottr',
+              style: TextStyle(
+                color: AppColors.primaryDark,
+                fontSize: 24.0,
+              ),
+            ),
+            actions: [
+              const IconButton(
+                  icon: Icon(
+                    Icons.check_box_rounded,
+                    color: AppColors.primaryDark,
+                  ),
+                  onPressed: null),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                child: GestureDetector(
+                  onTap: () {
+                    context.bloc<AuthBloc>().add(const AuthEvent.signedOut());
+                  },
+                  child: const CircleAvatar(),
+                ),
+              ),
+            ],
+            elevation: 0.0,
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              //TODO: Navigate to NoteFormScreen
+            },
+            child: const Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+          ),
         ),
       ),
     );
