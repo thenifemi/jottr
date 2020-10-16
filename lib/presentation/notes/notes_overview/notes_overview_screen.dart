e336ab44f4ab8fe5dedf57bf20288be9b9bebdc7
Nifemi Omotosho-Diffu <thenifemi@gmail.com>
2020-10-16 11:57:59 -0300

    .

---
+++ lib/presentation/notes/notes_overview/notes_overview_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class NotesOverviewScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
