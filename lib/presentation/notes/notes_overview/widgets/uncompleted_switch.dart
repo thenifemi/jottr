91a84fe3ca9dbf241c25faa91bbef9bb76acfd03
Nifemi Omotosho-Diffu <thenifemi@gmail.com>
2020-10-19 14:05:03 -0300

    .

--- lib/presentation/notes/notes_overview/widgets/uncompleted_switch.dart
+++ lib/presentation/notes/notes_overview/widgets/uncompleted_switch.dart
@@ -1,4 +1,6 @@
+import 'package:Jottr/application/notes/note_watcher/note_watcher_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 class UncompletedSwitch extends HookWidget {
@@ -9,11 +11,25 @@ class UncompletedSwitch extends HookWidget {
     return InkResponse(
       onTap: () {
         toggleState.value = !toggleState.value;
+        context.bloc<NoteWatcherBloc>().add(toggleState.value
+            ? const NoteWatcherEvent.watchUncompletedStarted()
+            : const NoteWatcherEvent.watchAllStarted());
       },
-      child: Icon(
-        toggleState.value
-            ? Icons.check_box_outline_blank
-            : Icons.indeterminate_check_box_rounded,
+      child: AnimatedSwitcher(
+        duration: const Duration(milliseconds: 300),
+        transitionBuilder: (child, animation) => ScaleTransition(
+          scale: animation,
+          child: child,
+        ),
+        child: toggleState.value
+            ? const Icon(
+                Icons.check_box_outline_blank_rounded,
+                key: Key('outline'),
+              )
+            : const Icon(
+                Icons.indeterminate_check_box_rounded,
+                key: Key('indeterminate'),
+              ),
       ),
     );
   }
