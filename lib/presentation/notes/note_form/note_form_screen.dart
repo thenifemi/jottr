38319c854d06f22430fed9fad43d9d012bc30ba1
Nifemi Omotosho-Diffu <thenifemi@gmail.com>
2020-10-20 11:15:38 -0300

    .

--- lib/presentation/notes/note_form/note_form_screen.dart
+++ lib/presentation/notes/note_form/note_form_screen.dart
@@ -1,1 +1,16 @@
+import 'package:Jottr/domain/notes/note.dart';
+import 'package:flutter/material.dart';
 
+class NoteFormScreen extends StatelessWidget {
+  final Note editednote;
+
+  const NoteFormScreen({
+    Key key,
+    @required this.editednote,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
