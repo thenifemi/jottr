f087c887b503475906933bd8c4293d4cec1f8957
Nifemi Omotosho-Diffu <thenifemi@gmail.com>
2020-10-22 20:40:31 -0300

    Todo tile

--- lib/presentation/notes/note_form/widgets/add_todo_tile_widget.dart
+++ lib/presentation/notes/note_form/widgets/add_todo_tile_widget.dart
@@ -3,6 +3,8 @@ import 'package:Jottr/presentation/notes/note_form/misc/todo_item_presentation_c
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kt_dart/kt.dart';
+import 'package:provider/provider.dart';
+import 'package:Jottr/presentation/notes/note_form/misc/build_context_x.dart';
 
 class AddTodoTile extends StatelessWidget {
   const AddTodoTile({
@@ -11,18 +13,37 @@ class AddTodoTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: const Text('Add todo'),
-      leading: const Padding(
-        padding: EdgeInsets.all(10.0),
-        child: Icon(Icons.add),
-      ),
-      onTap: () {
-        context.bloc<NoteFormBloc>().add(
-              NoteFormEvent.todosChanged(listOf(
-                TodoItemPrimitive.empty(),
-              )),
+    return BlocConsumer<NoteFormBloc, NoteFormState>(
+      listenWhen: (previous, current) =>
+          previous.isEditing != current.isEditing,
+      listener: (context, state) {
+        context.formTodos = state.note.todos.value.fold(
+          (_) => listOf<TodoItemPrimitive>(),
+          (todoItemList) => todoItemList.map(
+            (_) => TodoItemPrimitive.fromDomain(_),
+          ),
+        );
+      },
+      buildWhen: (previous, current) =>
+          previous.note.todos.isfull != current.note.todos.isfull,
+      builder: (context, state) {
+        return ListTile(
+          enabled: !state.note.todos.isfull,
+          title: const Text('Add todo'),
+          leading: const Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Icon(Icons.add),
+          ),
+          onTap: () {
+            context.formTodos = context.formTodos.plusElement(
+              TodoItemPrimitive.empty(),
             );
+
+            context.bloc<NoteFormBloc>().add(
+                  NoteFormEvent.todosChanged(context.formTodos),
+                );
+          },
+        );
       },
     );
   }
