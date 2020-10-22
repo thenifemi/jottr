f5aa61a492f78dc00c49b48ce6946f6585067984
Nifemi Omotosho-Diffu <thenifemi@gmail.com>
2020-10-22 20:41:29 -0300

    extension

---
+++ lib/presentation/notes/note_form/misc/build_context_x.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+import 'package:kt_dart/collection.dart';
+import 'package:provider/provider.dart';
+
+import 'todo_item_presentation_classes.dart';
+
+extension FormTodosX on BuildContext {
+  KtList<TodoItemPrimitive> get formTodos =>
+      Provider.of<FormTodos>(this, listen: false).value;
+
+  set formTodos(KtList<TodoItemPrimitive> value) =>
+      Provider.of<FormTodos>(this, listen: false).value = value;
+}
